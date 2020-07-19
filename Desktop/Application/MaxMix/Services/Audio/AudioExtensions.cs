8b31d02f6cc61bb69cb2cacaac3caf7b1da4f4b1
Ryan Caltabiano <rcalt2vt@gmail.com>
2020-07-19 16:15:18 -0500

    Using less access privileges to get file name.

--- Desktop/Application/MaxMix/Services/Audio/AudioExtensions.cs
+++ Desktop/Application/MaxMix/Services/Audio/AudioExtensions.cs
@@ -10,11 +10,20 @@ namespace MaxMix.Services.Audio
         [DllImport("Kernel32.dll")]
         private static extern bool QueryFullProcessImageName([In] IntPtr hProcess, [In] uint dwFlags, [Out] StringBuilder lpExeName, [In, Out] ref uint lpdwSize);
 
+        #region Process Acess Flags
+        private const int PROCESS_QUERY_LIMITED_INFORMATION = 0x1000;
+        private const int SYNCHRONIZE = 0x00100000;
+        #endregion
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(int access, bool inheritHandle, int processID);
+
         public static string GetMainModuleFileName(this Process process, int buffer = 1024)
         {
             var fileNameBuilder = new StringBuilder(buffer);
             uint bufferLength = (uint)fileNameBuilder.Capacity + 1;
-            return QueryFullProcessImageName(process.Handle, 0, fileNameBuilder, ref bufferLength) ? fileNameBuilder.ToString() : null;
+            IntPtr handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION | SYNCHRONIZE, false, process.Id);
+            return QueryFullProcessImageName(handle, 0, fileNameBuilder, ref bufferLength) ? fileNameBuilder.ToString() : null;
         }
     }
 }
