5c7216a989742b7c23754ec5a2450d2cac001e60
Ruben Henares <rhenares0@gmail.com>
2020-08-31 13:40:02 -0700

    Updating build script to use choco packages

--- BuildSystem/scripts/build-all.cmd
+++ BuildSystem/scripts/build-all.cmd
@@ -1,8 +1,13 @@
-@echo off
-echo ==== MaxMix Project Auto Build ====
-pause
+:: =================================================
+:: This script compiles the 2 versions of the firmware
+:: for different rotary encoder condfigurations (full/half step)
+:: It assumes the arduino-cli is installed and added to the path.
+:: If you haven't done so, run install-dependencies.cmd first.
+:: =================================================
 
-set BIN_DIR=.
+@echo off
+echo ==== MaxMix Project Firmware Builder ====
+
 set ROOT_DIR=%CD%\..
 set BUILD_DIR=%ROOT_DIR%\build
 set RELEASE_DIR=%ROOT_DIR%\release
@@ -12,11 +17,11 @@ rmdir /s /q %RELEASE_DIR%
 mkdir %RELEASE_DIR%
 
 rmdir /s /q %BUILD_DIR%
-%BIN_DIR%\arduino-cli.exe compile -b arduino:avr:nano:cpu=atmega328 -v --build-path %BUILD_DIR% %MAIN_INO%
+arduino-cli compile -b arduino:avr:nano:cpu=atmega328 -v --build-path %BUILD_DIR% %MAIN_INO%
 move %BUILD_DIR%\MaxMix.ino.hex %RELEASE_DIR%\MaxMix.ino.hex 
 
 rmdir /s /q  %BUILD_DIR%
-%BIN_DIR%\arduino-cli.exe compile -b arduino:avr:nano:cpu=atmega328 -v --build-path %BUILD_DIR% --build-properties build.extra_flags=-DHALF_STEP %MAIN_INO%
+arduino-cli compile -b arduino:avr:nano:cpu=atmega328 -v --build-path %BUILD_DIR% --build-properties build.extra_flags=-DHALF_STEP %MAIN_INO%
 move %BUILD_DIR%\MaxMix.ino.hex %RELEASE_DIR%\MaxMix.halfstep.ino.hex 
 
 rmdir /s /q %BUILD_DIR%
