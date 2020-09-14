d705da16a191791855cf3f85b0dfc66192081f20
Ryan Caltabiano <rcalt2vt@gmail.com>
2020-09-14 17:22:34 -0500

    CS Test Project

---
+++ Desktop/Application/MaxMixTest/Messages.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MaxMixTest
+{
+    public enum Command
+    {
+        TEST = 1,
+        OK,
+        SETTINGS,
+        SESSION_INFO,
+        CURRENT,
+        PREVIOUS,
+        NEXT,
+        VOLUME,
+        SCREEN,
+        DEBUG
+    }
+
+    static class MessageUtils
+    {
+        public static bool AreEqual(this IMessage message, IMessage other)
+        {
+            var bytes = message.GetBytes();
+            var otherBytes = other.GetBytes();
+            return Enumerable.SequenceEqual(bytes, otherBytes);
+        }
+
+        public static void Extract(this byte data, ref byte value1, ref bool value2)
+        {
+            value1 = (byte)(data & 0x7F);
+            value2 = (data & 0x80) == 0x80;
+        }
+
+        public static void Pack(ref this byte data, byte value1, bool value2)
+        {
+            data = (byte)(value1 | (value2 ? 0x80 : 0x00));
+        }
+    }
+
+
+    public interface IMessage
+    {
+        byte[] GetBytes();
+        void SetBytes(byte[] bytes);
+    }
+
+    public struct SessionInfo : IMessage, IEquatable<SessionInfo>
+    {
+        public byte current;
+        public byte count;
+
+        public static SessionInfo Default()
+        {
+            return new SessionInfo
+            {
+                count = 0,
+                current = 0
+            };
+        }
+
+        public bool Equals(SessionInfo other)
+        {
+            return this.AreEqual(other);
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] buffer = new byte[2];
+            buffer[0] = current;
+            buffer[1] = count;
+            return buffer;
+        }
+
+        public void SetBytes(byte[] bytes)
+        {
+            Debug.Assert(bytes.Length == 2);
+            current = bytes[0];
+            count = bytes[1];
+        }
+    }
+
+    public struct Volume : IMessage, IEquatable<Volume>
+    {
+        internal byte _id;
+        public byte id
+        {
+            get => _id;
+            set => _id = Math.Min(value, (byte)127);
+        }
+
+        public bool isDefault;
+
+        internal byte _volume;
+        public byte volume
+        {
+            get => _volume;
+            set => _volume = Math.Min(value, (byte)100);
+        }
+
+        public bool isMuted;
+
+        public static Volume Default()
+        {
+            return new Volume
+            {
+                _id = 0,
+                isDefault = false,
+                _volume = 0,
+                isMuted = false
+            };
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] buffer = new byte[2];
+            buffer[0].Pack(_id, isDefault);
+            buffer[1].Pack(_volume, isMuted);
+            return buffer;
+        }
+
+        public void SetBytes(byte[] bytes)
+        {
+            Debug.Assert(bytes.Length == 2);
+            _id = (byte)(bytes[0] & 0x7F);
+            isDefault = (bytes[0] & 0x80) == 0x80;
+            _volume = (byte)(bytes[1] & 0x7F);
+            isMuted = (bytes[1] & 0x80) == 0x80;
+        }
+
+        public bool Equals(Volume other)
+        {
+            return this.AreEqual(other);
+        }
+    }
+
+    public struct Session : IMessage, IEquatable<Session>
+    {
+        internal char[] _name;
+        public string name
+        {
+            get => new string(_name);
+            set
+            {
+                _name = value.ToCharArray(0, 30);
+                _name[29] = (char)0;
+            }
+        }
+
+        public Volume values;
+
+        public static Session Default()
+        {
+            return new Session
+            {
+                _name = new char[30],
+                values = Volume.Default()
+            };
+        }
+
+        public byte[] GetBytes()
+        {
+            if (_name == null)
+                _name = new char[30];
+
+            byte[] buffer = new byte[32];
+            for (int i = 0; i < _name.Length; i++)
+                buffer[i] = (byte)_name[i];
+            buffer[30].Pack(values._id, values.isDefault);
+            buffer[31].Pack(values._volume, values.isMuted);
+            return buffer;
+        }
+
+        public void SetBytes(byte[] bytes)
+        {
+            Debug.Assert(bytes.Length == 32);
+            if (_name == null)
+                _name = new char[30];
+
+            for (int i = 0; i < 30; i++)
+                _name[i] = (char)bytes[i];
+
+            bytes[30].Extract(ref values._id, ref values.isDefault);
+            bytes[31].Extract(ref values._volume, ref values.isMuted);
+        }
+
+        public bool Equals(Session other)
+        {
+            return this.AreEqual(other);
+        }
+    }
+
+    public struct Screen : IMessage, IEquatable<Screen>
+    {
+        public byte id;
+
+        public static Screen Default()
+        {
+            return new Screen
+            {
+                id = 0
+            };
+        }
+
+        public byte[] GetBytes()
+        {
+            return new[] { id };
+        }
+
+        public void SetBytes(byte[] bytes)
+        {
+            Debug.Assert(bytes.Length == 1);
+            id = bytes[0];
+        }
+
+        public bool Equals(Screen other)
+        {
+            return this.AreEqual(other);
+        }
+    }
+
+    public struct Color : IEquatable<Color>
+    {
+        public byte r;
+        public byte g;
+        public byte b;
+
+        public static Color Default()
+        {
+            return new Color { r = 0, g = 0, b = 0, };
+        }
+
+        public Color(byte _r, byte _g, byte _b)
+        {
+            r = _r;
+            g = _g;
+            b = _g;
+        }
+
+        public bool Equals(Color other)
+        {
+            return r == other.r && g == other.g && b == other.b;
+        }
+    }
+
+    public struct Settings : IMessage, IEquatable<Settings>
+    {
+        public byte sleepAfterSeconds;
+
+        internal byte _accelerationPercentage;
+        public byte accelerationPercentage
+        {
+            get => _accelerationPercentage;
+            set => _accelerationPercentage = Math.Min(value, (byte)100);
+        }
+
+        public bool continuousScroll;
+        public Color volumeMinColor;
+        public Color volumeMaxColor;
+        public Color mixChannelAColor;
+        public Color mixChannelBColor;
+
+        public static Settings Default()
+        {
+            return new Settings
+            {
+                sleepAfterSeconds = 5,
+                accelerationPercentage = 60,
+                continuousScroll = true,
+                volumeMinColor = new Color(0, 0, 255),
+                volumeMaxColor = new Color(255, 0, 0),
+                mixChannelAColor = new Color(0, 0, 255),
+                mixChannelBColor = new Color(255, 0, 255)
+            };
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] buffer = new byte[14];
+            buffer[0] = sleepAfterSeconds;
+            buffer[1].Pack(_accelerationPercentage, continuousScroll);
+            buffer[2] = volumeMinColor.r;
+            buffer[3] = volumeMinColor.g;
+            buffer[4] = volumeMinColor.b;
+            buffer[5] = volumeMaxColor.r;
+            buffer[6] = volumeMaxColor.g;
+            buffer[7] = volumeMaxColor.b;
+            buffer[8] = mixChannelAColor.r;
+            buffer[9] = mixChannelAColor.g;
+            buffer[10] = mixChannelAColor.b;
+            buffer[11] = mixChannelBColor.r;
+            buffer[12] = mixChannelBColor.g;
+            buffer[13] = mixChannelBColor.b;
+            return buffer;
+        }
+
+        public void SetBytes(byte[] bytes)
+        {
+            Debug.Assert(bytes.Length == 14);
+            sleepAfterSeconds = bytes[0];
+
+            bytes[1].Extract(ref _accelerationPercentage, ref continuousScroll);
+
+            volumeMinColor = new Color(bytes[2], bytes[3], bytes[4]);
+            volumeMaxColor = new Color(bytes[5], bytes[6], bytes[7]);
+            mixChannelAColor = new Color(bytes[8], bytes[9], bytes[10]);
+            mixChannelBColor = new Color(bytes[11], bytes[12], bytes[13]);
+        }
+
+        public bool Equals(Settings other)
+        {
+            return this.AreEqual(other);
+        }
+    }
+}
---
+++ Desktop/Application/MaxMixTest/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+
+namespace MaxMixTest
+{
+    class Program
+    {
+        // Device state data
+        static Settings _settings = Settings.Default();
+        static SessionInfo _sessionInfo = SessionInfo.Default();
+        static Session[] _session = new[] { Session.Default(), Session.Default(), Session.Default() };
+        static Screen _screen = Screen.Default();
+
+
+        static SerialPort _serialPort;
+
+        static bool _allClear = false;
+        static ulong _readBytes = 0;
+        static ulong _readCount = 0;
+        static ulong _writeCount = 0;
+        static ulong _writeBytes = 0;
+        static ulong _errorCount = 0;
+
+
+        static Random _rand = new Random(Guid.NewGuid().GetHashCode());
+
+        static DateTime _lastDebug = DateTime.Now;
+        static TimeSpan _debugDelay = new TimeSpan(0, 0, 0, 1);
+        static DateTime _lastPrint = DateTime.Now;
+        static TimeSpan _printDelay = new TimeSpan(0, 0, 30);
+
+        static readonly string[] _sizeSuffixes = { "B", "KB", "MB", "GB", "TB", "PB" };
+        public static string ToSize(ulong bytes)
+        {
+            int counter = 0;
+            decimal number = bytes;
+            while (Math.Round(number / 1024) >= 1)
+            {
+                number = number / 1024;
+                counter++;
+            }
+            return $"{number:n1}{_sizeSuffixes[counter]}";
+        }
+
+
+        static void Main(string[] args)
+        {
+            _serialPort = new SerialPort("COM5", 115200);
+            _serialPort.ReadTimeout = 10;
+            _serialPort.WriteTimeout = 10;
+            _serialPort.Open();
+
+            Stopwatch timer = Stopwatch.StartNew();
+            Write(Command.TEST);
+            while (true)
+            {
+                Read();
+
+                if (_allClear)
+                {
+                    var now = DateTime.Now;
+                    if (now - _lastDebug > _debugDelay)
+                    {
+                        _lastDebug = now;
+                        Write(Command.DEBUG);
+                    }
+                    else if (now - _lastPrint > _printDelay)
+                    {
+                        _lastPrint = now;
+                        double readBps = _readBytes / timer.Elapsed.TotalSeconds;
+                        double writeBps = _writeBytes / timer.Elapsed.TotalSeconds;
+                        Console.WriteLine($"Read {ToSize(_readBytes)} @ {readBps:n1}B/s ({_readCount}), Write {ToSize(_writeBytes)} @ {writeBps:n1}B/s ({_writeCount}), Errors {_errorCount}");
+                    }
+                    else
+                    {
+                        // Do Test Fuzzing
+                        Write((Command)_rand.Next((int)Command.SETTINGS, (int)Command.DEBUG));
+                    }
+                }
+            }
+        }
+
+        static void RawReadDump()
+        {
+            if (_serialPort.BytesToRead > 0)
+            {
+                int command = _serialPort.ReadByte();
+                Console.WriteLine($"{command}");
+            }
+        }
+
+        static void ReadMessage<T>(T oldMessage) where T : IMessage, IEquatable<T>, new()
+        {
+            var message = new T();
+            var buffer = message.GetBytes();
+
+            int length = 0;
+            while (length < buffer.Length)
+                length += _serialPort.Read(buffer, length, buffer.Length - length);
+
+            _readBytes += (uint)length;
+
+            if (length != buffer.Length)
+                _errorCount++;
+
+            message.SetBytes(buffer);
+            if (!oldMessage.Equals(message))
+                _errorCount++;
+        }
+
+        static void Read()
+        {
+            if (_serialPort.BytesToRead > 0)
+            {
+                int command = _serialPort.ReadByte();
+                _readCount++;
+                _readBytes++;
+
+                switch ((Command)command)
+                {
+                    case Command.TEST:
+                        {
+                            var firmware = _serialPort.ReadLine();
+                            Console.WriteLine($"Firmware: {firmware}");
+                        }
+                        break;
+                    case Command.OK:
+                        _allClear = true;
+                        break;
+                    case Command.SETTINGS:
+                        ReadMessage(_settings);
+                        break;
+                    case Command.SESSION_INFO:
+                        ReadMessage(_sessionInfo);
+                        break;
+                    case Command.CURRENT:
+                        ReadMessage(_session[1]);
+                        break;
+                    case Command.PREVIOUS:
+                        ReadMessage(_session[0]);
+                        break;
+                    case Command.NEXT:
+                        ReadMessage(_session[2]);
+                        break;
+                    case Command.VOLUME:
+                        ReadMessage(_session[1].values);
+                        break;
+                    case Command.SCREEN:
+                        ReadMessage(_screen);
+                        break;
+                    case Command.DEBUG:
+                        _errorCount++; // Do Nothing, but this is an error as we should never get this msg
+                        break;
+                }
+            }
+        }
+
+        static void WriteMessage(Command command, IMessage message = null)
+        {
+            byte[] bytes;
+            if (message != null)
+            {
+                byte[] payload = message.GetBytes();
+                bytes = new byte[payload.Length + 1];
+                Array.Copy(payload, 0, bytes, 1, payload.Length);
+            }
+            else
+            {
+                bytes = new byte[1];
+            }
+
+            bytes[0] = (byte)command;
+            _writeBytes += (uint)bytes.Length;
+            _serialPort.Write(bytes, 0, bytes.Length);
+        }
+
+        static void WriteFuzzMessage<T>(Command command, ref T message) where T : IMessage
+        {
+            byte[] payload = message.GetBytes();
+            _rand.NextBytes(payload);
+            if (message is Session)
+                payload[29] = 0; // Expected to have null terminated strings
+            message.SetBytes(payload);
+
+            byte[] bytes = new byte[payload.Length + 1];
+            Array.Copy(payload, 0, bytes, 1, payload.Length);
+
+            bytes[0] = (byte)command;
+            _writeBytes += (uint)bytes.Length;
+            _serialPort.Write(bytes, 0, bytes.Length);
+        }
+
+        static void Write(Command command)
+        {
+            _allClear = false;
+            _writeCount++;
+            switch (command)
+            {
+                case Command.TEST:
+                    WriteMessage(command);
+                    break;
+                case Command.OK:
+                    WriteMessage(command);
+                    break;
+                case Command.SETTINGS:
+                    WriteFuzzMessage(command, ref _settings);
+                    break;
+                case Command.SESSION_INFO:
+                    WriteFuzzMessage(command, ref _sessionInfo);
+                    break;
+                case Command.CURRENT:
+                    WriteFuzzMessage(command, ref _session[1]);
+                    break;
+                case Command.PREVIOUS:
+                    WriteFuzzMessage(command, ref _session[0]);
+                    break;
+                case Command.NEXT:
+                    WriteFuzzMessage(command, ref _session[2]);
+                    break;
+                case Command.VOLUME:
+                    WriteFuzzMessage(command, ref _session[1].values);
+                    break;
+                case Command.SCREEN:
+                    WriteFuzzMessage(command, ref _screen);
+                    break;
+                case Command.DEBUG:
+                    WriteMessage(command);
+                    break;
+            }
+        }
+    }
+}
