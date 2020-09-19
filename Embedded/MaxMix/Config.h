46af809325a78a8a4995a484636d653fc9640104
GIT-Desktop <relations99@gmail.com>
2020-09-19 18:31:33 +0800

    Update FW Version

--- Embedded/MaxMix/Config.h
+++ Embedded/MaxMix/Config.h
@@ -17,11 +17,11 @@
 // *** DEFINES
 //********************************************************
 #ifndef VERSION_MAJOR
-    #define VERSION_MAJOR 0
+    #define VERSION_MAJOR 1
 #endif
 
 #ifndef VERSION_MINOR
-    #define VERSION_MINOR 0
+    #define VERSION_MINOR 3
 #endif
 
 #ifndef VERSION_PATCH
