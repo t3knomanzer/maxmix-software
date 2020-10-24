72d66f056e0e64c9e5932019ebedd346f35ddfb8
Ryan Caltabiano <rcalt2vt@gmail.com>
2020-10-24 07:26:38 -0500

    Crash Fix #221

--- Desktop/Application/MaxMix/ViewModels/MainViewModel.cs
+++ Desktop/Application/MaxMix/ViewModels/MainViewModel.cs
@@ -211,7 +211,7 @@ namespace MaxMix.ViewModels
                 return;
 
             bool isCurrentMode = deviceFlow.ToDisplayMode() == m_SessionInfo.mode;
-            UpdateSessionData(id, isCurrentMode, true);
+            UpdateSessionData(id, isCurrentMode, true, deviceFlow.ToDisplayMode());
         }
 
         private void OnDeviceRemoved(object sender, int id, DeviceFlow deviceFlow)
@@ -220,7 +220,7 @@ namespace MaxMix.ViewModels
                 return;
 
             bool isCurrentMode = deviceFlow.ToDisplayMode() == m_SessionInfo.mode;
-            UpdateSessionData(id, isCurrentMode, false);
+            UpdateSessionData(id, isCurrentMode, false, deviceFlow.ToDisplayMode());
         }
 
         private void OnAudioSessionCreated(object sender, int id, string displayName, int volume, bool isMuted)
@@ -229,7 +229,7 @@ namespace MaxMix.ViewModels
                 return;
 
             bool isCurrentMode = m_SessionInfo.mode == DisplayMode.MODE_APPLICATION || m_SessionInfo.mode == DisplayMode.MODE_GAME;
-            UpdateSessionData(id, isCurrentMode, true);
+            UpdateSessionData(id, isCurrentMode, true, DisplayMode.MODE_APPLICATION);
         }
 
         private void OnAudioSessionRemoved(object sender, int id)
@@ -238,21 +238,35 @@ namespace MaxMix.ViewModels
                 return;
 
             bool isCurrentMode = m_SessionInfo.mode == DisplayMode.MODE_APPLICATION || m_SessionInfo.mode == DisplayMode.MODE_GAME;
-            UpdateSessionData(id, isCurrentMode, false);
+            UpdateSessionData(id, isCurrentMode, false, DisplayMode.MODE_APPLICATION);
         }
 
-        private void UpdateSessionData(int id, bool updateCurrent, bool addition)
+        private void UpdateSessionData(int id, bool updateCurrent, bool addition, DisplayMode updateMode)
         {
-            ISession[] sessions = _audioSessionService.GetSessions(m_SessionInfo.mode);
-            if (m_SessionInfo.mode == DisplayMode.MODE_INPUT)
+            ISession[] sessions = _audioSessionService.GetSessions(updateMode);
+            if (updateMode == DisplayMode.MODE_INPUT)
                 m_SessionInfo.input = (byte)sessions.Length;
-            else if (m_SessionInfo.mode == DisplayMode.MODE_OUTPUT)
+            else if (updateMode == DisplayMode.MODE_OUTPUT)
                 m_SessionInfo.output = (byte)sessions.Length;
             else
                 m_SessionInfo.application = (byte)sessions.Length;
 
             if (updateCurrent)
             {
+                // Do we still have sessions left?
+                if (sessions.Length == 0)
+                {
+                    m_SessionInfo.mode = (DisplayMode)((int)m_SessionInfo.mode + 1 % (int)DisplayMode.MODE_MAX);
+                    if (m_SessionInfo.mode == DisplayMode.MODE_SPLASH)
+                        m_SessionInfo.mode = DisplayMode.MODE_OUTPUT;
+                    m_SessionInfo.current = 0;
+
+                    UpdateSessionData(int.MaxValue, updateCurrent, addition, m_SessionInfo.mode);
+
+                    _communicationService.SendMessage(Command.SESSION_INFO, m_SessionInfo);
+                    return;
+                }
+
                 int currId = m_IndexToId[m_SessionInfo.current];
                 if (id <= currId)
                 {
