02d9d6617ff9e882aa1c5f6691bada8a7a4bc1cf
Ryan Caltabiano <rcalt2vt@gmail.com>
2020-10-25 14:25:08 -0500

    Version bump

--- Desktop/Application/MaxMix/Services/Communication/FirmwareVersions.cs
+++ Desktop/Application/MaxMix/Services/Communication/FirmwareVersions.cs
@@ -16,7 +16,8 @@ namespace MaxMix.Services.Communication
             "1.4.0",
             "1.4.1",
             "1.4.2",
-            "1.4.3"
+            "1.4.3",
+            "1.4.4"
         };
 
         public static bool IsCompatible(string version)
